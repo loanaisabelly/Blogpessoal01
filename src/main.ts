2813359c221ef70ffc2705b5e959cb60511e0996
loanaisabelly <loanaisabelly.7@gmail.com>
2025-02-19 16:01:53 -0300

    :ok_hand: Revisão e alterações no codigo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Loana Isabelly","https://github.com/loanaisabelly","loanaisabelly.7.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   process.env.tz = '-03-00'
+
+
+
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
