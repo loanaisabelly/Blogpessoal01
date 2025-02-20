aab56ac9c13ac9f3c5f1b0f267f9c51fc02d3081
loanaisabelly <loanaisabelly.7@gmail.com>
2025-02-20 14:33:16 -0300

    :ok_hand: Alterações para o CORS

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
-  .setTitle('Blog Pessoal')
-  .setDescription('Projeto Blog Pessoal')
-  .setContact("Loana Isabelly","https://github.com/loanaisabelly","loanaisabelly.7.com")
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact("Loana Isabelly", "https://github.com/loanaisabelly", "loanaisabelly.7.com")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
 
-  process.env.tz = '-03-00'
 
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204
+  };
 
+  app.enableCors(corsOptions); 
 
+  process.env.tz = '-03-00';
 
   await app.listen(process.env.PORT ?? 4000);
 }
